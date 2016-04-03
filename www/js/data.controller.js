3f2c80ad1420fae135cbdbb728ff562d14739029
DragoPlut <DragoPlut@bitbucket.org>
2016-04-03 23:43:03 +0300

    trubles with ng

---
+++ www/js/data.controller.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('myApp.data', [])
+
+  // .config(function(){
+  //   .state({
+  //     url: '/table',
+  //     controller: 'DataController',
+  //     controllerAs: 'data'
+  //   })
+  // });
+
+  .controller('dataController', function (){
+    var self = this;
+
+    self.drinkList = [{id: 1, drink: 'Black Tea', price: 10}, {id: 2, drink: 'Green Tea', price: 12}, {id: 3, drink: 'Coffee', price: 15}, {id: 4, drink: 'Juice', price: 9}];
+    self.listOfTitles = [{id: 1, title: 'Task1'}, {id: 2, title: 'Task2'}, {id: 3, title: 'Task3'}];
+    self.newTitle = '';
+
+    self.addTitle = function (){
+      self.listOfTitles.push({id: self.listOfTitles.length+1, title: self.newTitle});
+      self.newTitle = '';
+    }
+  });
