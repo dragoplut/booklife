f6185837295185ec718cc1fb73eb49bb533a9845
oleksandr.penkivskyy <oleksandr.penkivskyy@vakoms.gmail.com>
2016-04-04 09:55:03 +0300

    projects template from codepen

--- www/js/data.controller.js
+++ www/js/data.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.data', [])
+//angular.module('myApp.data', [])
 
   // .config(function(){
   //   .state({
@@ -10,15 +10,15 @@ angular.module('myApp.data', [])
   //   })
   // });
 
-  .controller('dataController', function (){
-    var self = this;
-
-    self.drinkList = [{id: 1, drink: 'Black Tea', price: 10}, {id: 2, drink: 'Green Tea', price: 12}, {id: 3, drink: 'Coffee', price: 15}, {id: 4, drink: 'Juice', price: 9}];
-    self.listOfTitles = [{id: 1, title: 'Task1'}, {id: 2, title: 'Task2'}, {id: 3, title: 'Task3'}];
-    self.newTitle = '';
-
-    self.addTitle = function (){
-      self.listOfTitles.push({id: self.listOfTitles.length+1, title: self.newTitle});
-      self.newTitle = '';
-    }
-  });
+  //.controller('dataController', function (){
+  //  var self = this;
+  //
+  //  self.drinkList = [{id: 1, drink: 'Black Tea', price: 10}, {id: 2, drink: 'Green Tea', price: 12}, {id: 3, drink: 'Coffee', price: 15}, {id: 4, drink: 'Juice', price: 9}];
+  //  self.listOfTitles = [{id: 1, title: 'Task1'}, {id: 2, title: 'Task2'}, {id: 3, title: 'Task3'}];
+  //  self.newTitle = '';
+  //
+  //  self.addTitle = function (){
+  //    self.listOfTitles.push({id: self.listOfTitles.length+1, title: self.newTitle});
+  //    self.newTitle = '';
+  //  }
+  //});
